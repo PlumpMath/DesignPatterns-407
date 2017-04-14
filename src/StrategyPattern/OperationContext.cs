fafeadf151a95545b1f5b68e0bf1bf8cf0c25c26
wddpct <1242450527@qq.com>
2017-04-14 15:25:35 +0800

    增加策略模式相关代码

---
+++ src/StrategyPattern/OperationContext.cs
@@ -0,0 +1,17 @@
+﻿namespace StrategyPattern
+{
+    public class OperationContext
+    {
+        private readonly IStrategy _strategy;
+
+        public OperationContext(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public int ExecuteStrategy(int num1, int num2)
+        {
+            return _strategy.DoOperation(num1, num2);
+        }
+    }
+}
---
+++ src/StrategyPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var context = new OperationContext(new OperationAdd());
+            Console.WriteLine("10 + 5 = " + context.ExecuteStrategy(10, 5));
+
+            context = new OperationContext(new OperationSubstract());
+            Console.WriteLine("10 - 5 = " + context.ExecuteStrategy(10, 5));
+
+            context = new OperationContext(new OperationMultiply());
+            Console.WriteLine("10 * 5 = " + context.ExecuteStrategy(10, 5));
+        }
+    }
+}
